07810e4ba2992fd580b63a8ae08f822ec9135d69
Federico Castañeda <fcastaneda@allurion.com>
2023-10-01 19:38:11 -0300

    exp07

--- src/workflow-inicial/601_RUN_correr_workflow_07.r
+++ src/workflow-inicial/601_RUN_correr_workflow_07.r
@@ -12,9 +12,9 @@ options(error = function() {
 # primeros pasos, relativamente rapidos
 # source("~/dm2023b/src/workflow-inicial/611_CA_reparar_dataset_02.r")
 # source("~/dm2023b/src/workflow-inicial/621_DR_corregir_drifting_02.r")
-source("~/dm2023b/src/workflow-inicial/631_FE_historia_02.r")
-source("~/dm2023b/src/workflow-inicial/641_TS_training_strategy_02.r")
+source("~/dm2023b/src/workflow-inicial/631_FE_historia_07.r")
+source("~/dm2023b/src/workflow-inicial/641_TS_training_strategy_07.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-inicial/651_HT_lightgbm_02.r")
-source("~/dm2023b/src/workflow-inicial/661_ZZ_final_02.r")
+source("~/dm2023b/src/workflow-inicial/651_HT_lightgbm_07.r")
+source("~/dm2023b/src/workflow-inicial/661_ZZ_final_07.r")
