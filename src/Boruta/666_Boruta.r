acfde0131b26cf24dd81201aa8613f4b3ec61427
Federico Castañeda <fcastaneda@allurion.com>
2023-10-06 16:47:01 -0300

    Boruta

--- src/Boruta/666_Boruta.r
+++ src/Boruta/666_Boruta.r
@@ -1,27 +1,37 @@
-# Experimentos Colaborativos Default
-# Workflow  Feature Engineering historico
-
-# limpio la memoria
-rm(list = ls(all.names = TRUE)) # remove all objects
-gc(full = TRUE) # garbage collection
-
+library(Boruta)
 require("data.table")
-require("yaml")
-require("Rcpp")
 
-require("ranger")
-require("randomForest") # solo se usa para imputar nulos
+setwd( "~/buckets/b1/")
+boruta_semilla = 800161
 
-require("lightgbm")
+# dataset_input <- paste0("./exp/", PARAM$exp_input, "/dataset.csv.gz")
+dataset_input <- paste0("./exp/DR6210/dataset.csv.gz")
+
+dataset <- fread(dataset_input)
+colnames(dataset)[which(!(sapply(dataset, typeof) %in% c("integer", "double")))]
+
+#setorder(dataset, numero_de_cliente, foto_mes)
+
+dataset[, clase01 := ifelse(clase_ternaria == "CONTINUA", 0, 1)]
+set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
+
+campos_buenos <- setdiff(
+  colnames(dataset),
+  c("clase_ternaria", "foto_mes")
+)
+
+set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
+azar <- runif(nrow(dataset))
+
+dataset[, entrenamiento := (clase01 == 1 | azar < 0.10)]
+
+dtrain <- dataset[  entrenamiento == TRUE ]
+
+ncol_antes <- ncol(dataset)
+
+boruta.train <- Boruta(
+  clase01~. ,
+  data = dataset[entrenamiento == TRUE, campos_buenos, with = FALSE],
+)
 
 
-# Parametros del script
-PARAM <- list()
-PARAM$experimento <- "FE6660-boruta-00"
-
-PARAM$exp_input <- "DR6210"
-
-PARAM$home <- "~/buckets/b1/"
-# FIN Parametros del script
-
-OUTPUT <- list()
