b430e66a2e833181f9a02b46106b7bc9d7101c38
Federico Castañeda <fcastaneda@allurion.com>
2023-10-06 17:29:10 -0300

    boruta

--- src/Boruta/666_Boruta.r
+++ src/Boruta/666_Boruta.r
@@ -1,11 +1,12 @@
 library(Boruta)
 require("data.table")
+require("randomForest")
 
 setwd( "~/buckets/b1/")
 boruta_semilla = 800161
 
 # dataset_input <- paste0("./exp/", PARAM$exp_input, "/dataset.csv.gz")
-dataset_input <- paste0("./exp/DR6210/dataset.csv.gz")
+dataset_input <- paste0("./datasets/dataset_pequeno.csv")
 
 dataset <- fread(dataset_input)
 colnames(dataset)[which(!(sapply(dataset, typeof) %in% c("integer", "double")))]
@@ -23,15 +24,21 @@ campos_buenos <- setdiff(
 set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
 azar <- runif(nrow(dataset))
 
-dataset[, entrenamiento := (clase01 == 1 | azar < 0.10)]
+dataset_rf <- copy(dataset[, campos_buenos, with = FALSE])
+set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
+azar <- runif(nrow(dataset_rf))
 
-dtrain <- dataset[  entrenamiento == TRUE ]
+dataset_rf[, entrenamiento :=
+             as.integer(clase01 == 1 | azar < 0.10)]
+
+set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
+dtrain <- na.roughfix(dataset_rf)
 
 ncol_antes <- ncol(dataset)
 
 boruta.train <- Boruta(
   clase01~. ,
-  data = dataset[entrenamiento == TRUE, campos_buenos, with = FALSE],
+  data = dtrain,
 )
 
 
