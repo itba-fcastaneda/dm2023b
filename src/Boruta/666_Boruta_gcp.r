d9216a0bfa6e81acd7ba3dd9a1955ead3956dd33
Federico Castañeda <fcastaneda@allurion.com>
2023-10-06 19:49:15 -0300

    boruta

--- src/Boruta/666_Boruta_gcp.r
+++ src/Boruta/666_Boruta_gcp.r
@@ -1,63 +1,68 @@
 library(Boruta)
 require("data.table")
 require("randomForest")
+require("yaml")
 
 PARAM <- list()
-PARAM$experimento <- "FE6310-boruta-01-small"
+PARAM$experimento <- "FE6310-boruta-02"
+
+PARAM$exp_input <- "DR6210"
+
+OUTPUT <- list()
 
 setwd( "~/buckets/b1/")
 boruta_semilla = 800161
 
-OUTPUT <- list()
-
-# dataset_input <- paste0("./exp/", PARAM$exp_input, "/dataset.csv.gz")
-dataset_input <- paste0("./datasets/dataset_pequeno.csv")
-
+dataset_input <- paste0("./exp/", PARAM$exp_input, "/dataset.csv.gz")
+#dataset_input <- paste0("./datasets/dataset_pequeno.csv")
 
 ############################################################
 GrabarOutput <- function() {
   write_yaml(OUTPUT, file = "output.yml") # grabo OUTPUT
 }
 
+# creo la carpeta donde va el experimento
+dir.create(paste0("./exp/", PARAM$experimento, "/"), showWarnings = FALSE)
+# Establezco el Working Directory DEL EXPERIMENTO
+setwd(paste0("./exp/", PARAM$experimento, "/"))
+
+############################################################
+
 dataset <- fread(dataset_input)
 colnames(dataset)[which(!(sapply(dataset, typeof) %in% c("integer", "double")))]
+
+
 
 #setorder(dataset, numero_de_cliente, foto_mes)
 
 dataset[, clase01 := ifelse(clase_ternaria == "CONTINUA", 0, 1)]
+
 set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
+azar <- runif(nrow(dataset))
 
-
+dataset[, entrenamiento :=
+             as.integer(foto_mes >= 202101 & foto_mes <= 202103 &
+                          (clase01 == 1 | azar < 0.10))]
 
 campos_buenos <- setdiff(
   colnames(dataset),
   c("clase_ternaria", "foto_mes")
 )
 
-set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
-azar <- runif(nrow(dataset))
+dataset_rf <- copy(dataset[entrenamiento == TRUE, campos_buenos, with = FALSE])
 
-dataset_rf <- copy(dataset[, campos_buenos, with = FALSE])
 set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
 azar <- runif(nrow(dataset_rf))
-
-dataset_rf[, entrenamiento :=
-             as.integer(clase01 == 1 | azar < 0.10)]
 
 set.seed(boruta_semilla, kind = "L'Ecuyer-CMRG")
 dtrain <- na.roughfix(dataset_rf)
 
 ncol_antes <- ncol(dataset)
 
-boruta.train <- Boruta(
+boruta_train <- Boruta(
   clase01~. ,
   data = dtrain,
 )
-
-# creo la carpeta donde va el experimento
-dir.create(paste0("./exp/", PARAM$experimento, "/"), showWarnings = FALSE)
-# Establezco el Working Directory DEL EXPERIMENTO
-setwd(paste0("./exp/", PARAM$experimento, "/"))
 
 boruta_attrs <- getSelectedAttributes(boruta_train, withTentative = FALSE)
 
@@ -66,6 +71,3 @@ boruta_fixed <- TentativeRoughFix(boruta.bank_train)
 OUTPUT$boruta_attrs <- boruta_attrs
 OUTPUT$boruta_fixed <- boruta_fixed
 GrabarOutput()
-
-
-
