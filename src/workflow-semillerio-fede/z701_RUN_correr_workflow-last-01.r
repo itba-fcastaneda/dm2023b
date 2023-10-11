7b5678b84e9302585563099125fd97888cc2a665
Federico Castañeda <fcastaneda@allurion.com>
2023-10-11 11:33:20 -0300

    exp01

---
+++ src/workflow-semillerio-fede/z701_RUN_correr_workflow-last-01.r
@@ -0,0 +1,22 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+#source("~/dm2023b/src/workflow-semillerio-fede/z711_CA_reparar_dataset-last-00.r")
+#source("~/dm2023b/src/workflow-semillerio-fede/z721_DR_corregir_drifting-last-00.r")
+source("~/dm2023b/src/workflow-semillerio-fede/z731_FE_historia-last-01.r")
+source("~/dm2023b/src/workflow-semillerio-fede/z741_TS_training_strategy-last-01.r")
+
+# ultimos pasos, muy lentos
+source("~/dm2023b/src/workflow-semillerio-fede/z751_HT_lightgbm-last-01.r")
+source("~/dm2023b/src/workflow-semillerio-fede/z795_ZZ_final_semillerio-last-01.r")
+source("~/dm2023b/src/workflow-semillerio-fede/z796_HB_semillerios_hibridacion-last-01.r")
+
